e75d5bed9474f14be43e6d6efba77157f069f919
unknown <daza.ronnie.09212002@gmail.com>
2022-11-19 11:40:46 +0800

    Add voice commit

--- virtual assistant.py
+++ virtual assistant.py
@@ -1,11 +1,21 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+engine.setProperty("voice", voices[1].id)
+voices = engine.getProperty("voices")
+
+engine.say("I am Alexa.")
+engine.say("What can I do for you?")
+engine.runAndWait()
 try:
     with sr.Microphone() as source:
         print("Listening...")
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
-        print(command)
+        command = command.lower()
+        if "alexa" in command:
+            print(command)
 except:
     pass
